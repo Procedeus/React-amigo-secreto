976f80a8117850049047b280c4ae688dd9abad5c
Murilo <murilorez79@hotmail.com>
2023-06-12 19:00:25 -0300

    Criação do botão 'Mostrar mais'

--- src/App.js
+++ src/App.js
@@ -6,20 +6,25 @@ import Tables from './Components/Tables/Tables/tables';
 
 function App() {
   const [tables, setTables] = useState([]);
-    
+  const [currentTable, setCurrentTable] = useState(2);
+  const [currentTables, setCurrentTables] = useState([]);
+
   useEffect(() =>{
+
     async function getTables(){
       const response = await api.get('/tables',);
       setTables(response.data);
+      setCurrentTables(tables.slice(0, currentTable));
     }
+
     getTables();
-  }, [])
+  }, [tables, currentTable])
 
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Tables setTables={setTables} tables={tables}/>} />
+          <Route path="/" element={<Tables setTables={setTables} tables={currentTables} currentTable={currentTable} setCurrentTable={setCurrentTable} legthTable={tables.length}/>} />
         </Routes>
       </Router>
     </>
--- src/Components/Tables/Tables/tables.js
+++ src/Components/Tables/Tables/tables.js
@@ -1,15 +1,35 @@
+import { useEffect, useState } from 'react';
 import TableCreate from '../TableCreate/tableCreate';
 import Table from '../Table/table';
+import './tables.css';
 
-function Tables({tables, setTables}) {
-    return (
-      <>
+function Tables({tables, setTables, currentTable, setCurrentTable, legthTable}) {
+  const [showButton, setShowButton] = useState(true);
+
+  useEffect(() =>{
+    console.log(legthTable, currentTable)
+    setShowButton(legthTable > currentTable)
+
+  }, [legthTable, currentTable])
+
+  function handleButtonClick(){
+    setCurrentTable(currentTable + 1);
+  }
+  return (
+    <>
       <TableCreate setTables={setTables} tables={tables} />
       {tables.map(table => (
         <Table key={table._id} table={table} setTables={setTables} tables={tables}/>
       ))}
-      </>
-      );
-    }
+      <div className='container-table'>
+        <div className='click'>
+        {showButton && (
+          <button id='showButton' type="button" onClick={() => {handleButtonClick()}}>Mostrar Mais</button>
+        )}
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default Tables;
