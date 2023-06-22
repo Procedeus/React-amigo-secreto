58047a16e91ec8b70fe2c13fdd964ed4e4679d54
Murilo <murilorez79@hotmail.com>
2023-06-22 18:11:00 -0300

    Adição da autorização JWT

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import api from './services/api';
 import Tables from './Components/Tables/Tables/tables';
 import Login from './Components/Login/login';
 import Signup from './Components/Signup/signup';
 
 function App() {
-  const [tables, setTables] = useState([]);
-  const [currentTable, setCurrentTable] = useState(2);
-  const [currentTables, setCurrentTables] = useState([]);
-
-  useEffect(() =>{
-
-    async function getTables(){
-      const response = await api.get('/tables',);
-      setTables(response.data);
-      setCurrentTables(tables.slice(0, currentTable));
-    }
-
-    getTables();
-  }, [tables, currentTable])
-
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Tables setTables={setTables} tables={currentTables} currentTable={currentTable} setCurrentTable={setCurrentTable} legthTable={tables.length}/>} />
+          <Route path="/" element={<Tables/>} />
           <Route path="/login" element={<Login/>} />
           <Route path="/signup" element={<Signup/>} />
         </Routes>
--- src/Components/Tables/Tables/tables.js
+++ src/Components/Tables/Tables/tables.js
@@ -1,28 +1,57 @@
 import { useEffect, useState } from 'react';
+import api from '../../../services/api'
 import TableCreate from '../TableCreate/tableCreate';
 import Table from '../Table/table';
+import {useNavigate} from 'react-router-dom'
 import './tables.css';
 
-function Tables({tables, setTables, currentTable, setCurrentTable, legthTable}) {
-  const [showButton, setShowButton] = useState(true);
+function Tables() {
+  const [tables, setTables] = useState([]);
+  const [currentTable, setCurrentTable] = useState(2);
+  const [currentTables, setCurrentTables] = useState([]);
+
+  const navigate = useNavigate();
 
   useEffect(() =>{
-    setShowButton(legthTable > currentTable)
+    
+    async function getTables(){
+      const token = localStorage.getItem('token');
+      try{
+        const response = await api.get('/tables',
+        { headers: {
+          'Authorization': `Bearer ${token}`
+        }});
+        setTables(response.data);
+      }
+      catch(error){
+        if (error.response) {
+          alert(`Erro ${error.response.status}: ${error.response.data.error}`);
+        } else {
+          alert('Erro desconhecido: '+ error);
+        }
+        navigate('/login');
+      }
+    }
 
-  }, [legthTable, currentTable])
+    getTables();
+  }, []);
+  
+  useEffect(()=>{
+    setCurrentTables(tables.slice(0, currentTable));
+  }, [tables, currentTable])
 
   function handleButtonClick(){
-    setCurrentTable(currentTable + 1);
+    setCurrentTable(prevCurrentTable => prevCurrentTable + 1);
   }
   return (
     <>
-      <TableCreate setTables={setTables} tables={tables} />
-      {tables.map(table => (
-        <Table key={table._id} table={table} setTables={setTables} tables={tables}/>
+      <TableCreate setTables={setTables} tables={currentTables} />
+      {currentTables.map(table => (
+        <Table key={table._id} table={table} setTables={setTables} tables={currentTables}/>
       ))}
       <div className='container-table'>
         <div className='click'>
-        {showButton && (
+        {currentTable < tables.length && (
           <button id='showButton' type="button" onClick={() => {handleButtonClick()}}>Mostrar Mais</button>
         )}
         </div>
