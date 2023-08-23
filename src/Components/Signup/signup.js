bbfb0c0c33a045c880e3eb6916a79919a8180c2d
Murilo <murilorez79@hotmail.com>
2023-08-23 15:29:17 -0300

    Atualização do design de signup

--- src/Components/Signup/signup.js
+++ src/Components/Signup/signup.js
@@ -3,6 +3,7 @@ import api from '../../services/api';
 import './signup.css';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { validateUsername, validatePassword } from '../../Utils/validadores';
+import img from '../../assets/gift.png';
 
 function Signup(){
 
@@ -35,25 +36,35 @@ function Signup(){
 
     return(
         <div className="container-signup">
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    name="username"
-                    placeholder="Usuário"
-                    onChange={e =>  setUsername(e.target.value) }
-                />
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="Senha"
-                    onChange={e =>  setPassword(e.target.value) }
-                />
-                <button 
-                type="submit"
-                disabled={!validateSignup(username, password)}
-                >Confirmar</button>
-            </form>
-            <NavLink to="/login">Voltar </NavLink >
+            <img src={img} alt="person gifted"/>
+            <div className="form-box-signup">
+                <h1>Cadastro</h1>
+                <form className="form-signup" onSubmit={handleSubmit}>
+                    <input
+                        className={validateUsername(username) ? 'correct' : 'wrong'}
+                        type="text"
+                        name="username"
+                        placeholder="Usuário"
+                        onChange={e =>  setUsername(e.target.value) }
+                    />
+                    <input
+                        className={validatePassword(password) ? 'correct' : 'wrong'}
+                        type="password"
+                        name="password"
+                        placeholder="Senha"
+                        onChange={e =>  setPassword(e.target.value) }
+                    />
+                    <button 
+                    type="submit"
+                    disabled={!validateSignup(username, password)}
+                    >Confirmar</button>
+                </form>
+                <div>
+                    <p>Já é registrado?
+                    <NavLink to="/login"> Clique aqui</NavLink >
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
